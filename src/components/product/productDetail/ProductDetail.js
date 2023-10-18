c8954cd6ade43552addd6182b3c332963f072c23
olubiyi09 <olubiyi09@gmail.com>
2023-10-19 03:33:57 +0400

    added dependency to my use effect in productDetails

--- src/components/product/productDetail/ProductDetail.js
+++ src/components/product/productDetail/ProductDetail.js
@@ -35,7 +35,7 @@ const ProductDetail = () => {
         if (isError) {
             console.log(message);
         }
-    }, [isLoggedIn, isError, message, dispatch])
+    }, [isLoggedIn, isError, message, dispatch, product])
 
 
     return (
